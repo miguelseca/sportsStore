4fa47b4bd58e573c2dda0353cdb0744e21b1b58d
Miguel Seca <miguel.seca@gmail.com>
2022-12-02 12:01:14 +0000

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { SearchComponent } from './shared/search/search.component';
+import { BannerComponent } from './shared/banner/banner.component';
+import { CategoriesComponent } from './shared/categories-banner/categories.component';
+import { ButtonModule } from 'primeng/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ProductListComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    SearchComponent,
+    BannerComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
