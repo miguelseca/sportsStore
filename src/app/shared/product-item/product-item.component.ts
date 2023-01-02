f0e73b370dc2d0bb1828c07d04b7c436f0bef071
Miguel Seca <miguel.seca@gmail.com>
2023-01-02 22:11:55 +0000

    docs added

--- src/app/shared/product-item/product-item.component.ts
+++ src/app/shared/product-item/product-item.component.ts
@@ -1,21 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { CartService } from 'src/app/services/cart.service';
 import CartItem from 'src/app/models/cartItem';
+import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
+  providers: [MessageService]
 })
-export class ProductItemComponent {
+export class ProductItemComponent{
   @Input() product!: Product;
 
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private messageService: MessageService
+    
   ) {}
 
-   
 
+   
   addToCart() {
    
     const cartItem: CartItem = {
@@ -23,5 +27,9 @@ export class ProductItemComponent {
       quantity: 1,
     };
     this.cartService.setCartItem(cartItem);
+    this.messageService.add({key: 'c', severity:'success', summary: 'Success', detail: 'Message Content'});
   }
+
+  onConfirm() {}
+  onCancel() {}
 }
